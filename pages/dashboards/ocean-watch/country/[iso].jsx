6bc5b27a782a2596c14460987244bb101f9c31aa
Gary Tempus Jr <gary.tempus@wri.org>
2024-03-29 15:46:00 -0400

    style(CoastalProfile): fix linting error

--- pages/dashboards/ocean-watch/country/[iso].jsx
+++ pages/dashboards/ocean-watch/country/[iso].jsx
@@ -507,10 +507,7 @@ export default function OceanWatchCountryProfilePage({ iso }) {
                 bgImage="/static/images/pages/app/banner-coral.jpg"
               >
                 <p className="-claim">Check out the Coral Reefs dashboards</p>
-                <a
-                  className="c-button -alt -primary"
-                  href="/dashboards/coral-reef-dashboards"
-                >
+                <a className="c-button -alt -primary" href="/dashboards/coral-reef-dashboards">
                   Coral Reefs
                 </a>
               </Banner>
