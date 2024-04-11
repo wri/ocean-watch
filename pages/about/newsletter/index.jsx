986eb04363718a423b05ea9452820df64ac81988
Gary Tempus <gtempus@users.noreply.github.com>
2024-04-11 18:00:24 -0400

    feat: Initial Import of `about` Page (#8)
    * feat: initial import of `about` page and its children

---
+++ pages/about/newsletter/index.jsx
@@ -0,0 +1,12 @@
+import { useMemo } from 'react';
+import { useRouter } from 'next/router';
+// components
+import LayoutNewsletter from 'layout/app/newsletter';
+
+export default function NewsletterPage() {
+  const { query } = useRouter();
+
+  const isOceanWatch = useMemo(() => query.origin === 'ocean-watch', [query]);
+
+  return <LayoutNewsletter isOceanWatch={isOceanWatch} />;
+}
