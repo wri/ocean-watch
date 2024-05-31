3da26050631061c3adcf27b0ec22064eef7f2c3b
Gary Tempus Jr <gary.tempus@wri.org>
2024-05-31 12:22:58 -0500

    chore(about): remove legacy `newsletter-thank-you` page

--- pages/about/newsletter-thank-you/index.jsx
+++
@@ -1,6 +0,0 @@
-// components
-import LayoutNewsletterThankYou from 'layout/app/newsletter-thank-you';
-
-export default function NewsletterThankYouPage() {
-  return <LayoutNewsletterThankYou />;
-}
