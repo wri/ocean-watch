5b3322234a63e23dec8ffa00818899ee0326a913
Gary Tempus Jr <gary.tempus@wri.org>
2024-06-03 18:47:15 -0400

    refactor(explore): remove unused `explore` components and styles

--- modules/index.js
+++ modules/index.js
@@ -2,12 +2,10 @@ import blogReducer from './blog/reducers';
 import dashboardsReducer from './dashboards/reducers';
 import partnersReducer from './partners/reducers';
 import staticPagesReducer from './static-pages/reducers';
-import exploreReducer from './explore';
 
 export default {
   blog: blogReducer,
   dashboards: dashboardsReducer,
   partners: partnersReducer,
   staticPages: staticPagesReducer,
-  explore: exploreReducer,
 };
