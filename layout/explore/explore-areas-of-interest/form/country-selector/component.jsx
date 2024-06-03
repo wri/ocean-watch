afe91e2df5d679a7f73c61fb5fd04c24b8cd0b05
Gary Tempus <gtempus@users.noreply.github.com>
2024-06-03 17:26:08 -0400

    Remove More Search Components (#37)
    * chore(search): remove more components related to unneeded search functionality

--- layout/explore/explore-areas-of-interest/form/country-selector/component.jsx
+++ layout/explore/explore-areas-of-interest/form/country-selector/component.jsx
@@ -1,35 +1,25 @@
-import React, {
-  useState,
-  useCallback,
-  useMemo,
-  useRef,
-  useEffect,
-} from 'react';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // components
-import SearchInput from 'components/ui/SearchInput';
 import Spinner from 'components/ui/Spinner';
 
 // hooks
 import useCountryList from 'hooks/country/country-list';
 
-const CountrySelector = ({
-  onClickCountry,
-}) => {
+const CountrySelector = ({ onClickCountry }) => {
   const searchBoxRef = useRef(null);
   const [search, setSearch] = useState('');
-  const handleSearch = useCallback((_search) => { setSearch(_search); }, [setSearch]);
-  const getSearchBoxRef = useCallback((ref) => { searchBoxRef.current = ref; }, []);
-  const handleCountry = useCallback(({ target }) => {
-    const {
-      dataset: {
-        geostore,
-        name,
-      },
-    } = target;
-    onClickCountry({ name, geostore });
-  }, [onClickCountry]);
+
+  const handleCountry = useCallback(
+    ({ target }) => {
+      const {
+        dataset: { geostore, name },
+      } = target;
+      onClickCountry({ name, geostore });
+    },
+    [onClickCountry],
+  );
   const {
     data: countries,
     isFetching,
@@ -40,17 +30,24 @@ const CountrySelector = ({
     keepPreviousData: true,
   });
 
-  const countryList = useMemo(() => countries
-    .filter(({ name }) => !!name)
-    .map(({ name, geostoreId }) => ({
-      name,
-      geostoreId,
-    })),
-  [countries]);
+  const countryList = useMemo(
+    () =>
+      countries
+        .filter(({ name }) => !!name)
+        .map(({ name, geostoreId }) => ({
+          name,
+          geostoreId,
+        })),
+    [countries],
+  );
 
-  const results = useMemo(() => countryList
-    .filter(({ name }) => name.toLocaleLowerCase().includes(search.toLocaleLowerCase())),
-  [countryList, search]);
+  const results = useMemo(
+    () =>
+      countryList.filter(({ name }) =>
+        name.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+      ),
+    [countryList, search],
+  );
 
   useEffect(() => {
     if (searchBoxRef.current) searchBoxRef.current.focus();
@@ -58,30 +55,12 @@ const CountrySelector = ({
 
   return (
     <div className="c-country-search-selector">
-      <div className="search-container">
-        <SearchInput
-          getRef={getSearchBoxRef}
-          input={{
-            value: search,
-            placeholder: 'Type a country',
-          }}
-          onSearch={handleSearch}
-        />
-      </div>
       <div className="countries-container">
-        {(isInitialData && isFetching) && (
-          <Spinner
-            isLoading
-            className="-small -transparent"
-          />
-        )}
+        {isInitialData && isFetching && <Spinner isLoading className="-small -transparent" />}
         {isSuccess && (
           <ul className="country-list">
             {results.map(({ name, geostoreId }) => (
-              <li
-                key={geostoreId}
-                className="country-list-item"
-              >
+              <li key={geostoreId} className="country-list-item">
                 <button
                   type="button"
                   onClick={handleCountry}
--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,3 @@
-import searchReducer from 'layout/search/search-reducers';
 import blogReducer from './blog/reducers';
 import dashboardsReducer from './dashboards/reducers';
 import partnersReducer from './partners/reducers';
@@ -6,7 +5,6 @@ import staticPagesReducer from './static-pages/reducers';
 import exploreReducer from './explore';
 
 export default {
-  search: searchReducer,
   blog: blogReducer,
   dashboards: dashboardsReducer,
   partners: partnersReducer,
